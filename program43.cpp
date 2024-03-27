96d7142d6b5043da30fbce68a74171aaad512089
kylamae24 <158233606+kylamae24@users.noreply.github.com>
2024-03-26 22:49:31 -0400

    Add files via upload

---
+++ program43.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() { 
+   int p, *ptr, size; 
+   
+   ptr = &p; 
+   
+   
+   cout<<"Enter an integer: "; 
+   cin>>*ptr; 
+   cin.ignore(); 
+   
+   size = sizeof(*ptr); 
+   
+   cout<<"\nValue: "<<*ptr; 
+   cout<<"\nAddress: "<<&p; 
+   cout<<"\nSize in bytes: "<<size<<"\n"; 
+   
+   return 0; 
+   
+   }
